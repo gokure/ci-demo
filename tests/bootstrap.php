b3a2e7343c4c4acfefbe173db2eba7d864a2727d
Gang Wu <gokure@gmail.com>
2022-03-02 10:41:59 +0800

    Skip test hyperf 1.x :(

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,23 +10,21 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 use Hyperf\Config\Listener\RegisterPropertyHandlerListener;
+use Hyperf\Di\Aop\AstVisitorRegistry;
 use Hyperf\Di\Aop\PropertyHandlerVisitor;
 use Hyperf\Di\Aop\ProxyCallVisitor;
+use Hyperf\Di\Aop\RegisterInjectPropertyHandler;
 
 ! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
 ! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
 
 require_once BASE_PATH . '/vendor/autoload.php';
 
-if (class_exists(\Hyperf\Di\Aop\AstVisitorRegistry::class)) {
-    // Register AST visitors to the collector.
-    \Hyperf\Di\Aop\AstVisitorRegistry::insert(PropertyHandlerVisitor::class, PHP_INT_MAX / 2);
-    \Hyperf\Di\Aop\AstVisitorRegistry::insert(ProxyCallVisitor::class, PHP_INT_MAX / 2);
-}
+// Register AST visitors to the collector.
+AstVisitorRegistry::insert(PropertyHandlerVisitor::class, PHP_INT_MAX / 2);
+AstVisitorRegistry::insert(ProxyCallVisitor::class, PHP_INT_MAX / 2);
 
-if (class_exists(\Hyperf\Di\Aop\RegisterInjectPropertyHandler::class)) {
-    // Register Property Handler.
-    \Hyperf\Di\Aop\RegisterInjectPropertyHandler::register();
-}
+// Register Property Handler.
+RegisterInjectPropertyHandler::register();
 
 (new RegisterPropertyHandlerListener())->process(new \stdClass());
